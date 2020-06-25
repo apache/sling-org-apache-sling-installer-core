3759e38209577df181ba78a77a2ce61a754c8843
Konrad Windszus <kwin@apache.org>
2020-06-25 15:21:53 +0200

    SLING-9031 SLING-8548 helper class for disjunctive run modes and not operators

---
+++ src/test/java/org/apache/sling/installer/api/provider/RunModeSupportTest.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.installer.api.provider;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RunModeSupportTest {
+
+    @Test
+    public void testGetMatchingRunmodesFromName() {
+        Assert.assertEquals(0, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4", Collections.singleton("test5")));
+        Assert.assertEquals(0, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4", Stream.of("test1", "test3").collect(Collectors.toSet())));
+        Assert.assertEquals(0, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4", Stream.of("test2", "test3").collect(Collectors.toSet())));
+        Assert.assertEquals(2, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4", Stream.of("test1", "test2").collect(Collectors.toSet())));
+        Assert.assertEquals(2, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4", Stream.of("test2", "test4").collect(Collectors.toSet())));
+        Assert.assertEquals(3, RunModeSupport.getMatchingRunmodesFromDisjunctions("test1.test2,-test3.test4,test5.test6.test7", Stream.of("test1", "test2", "test4", "test5", "test6", "test7").collect(Collectors.toSet())));
+    }
+}
